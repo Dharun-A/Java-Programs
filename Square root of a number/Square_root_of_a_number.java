d9dd6dbe868375493e0abba63c365773688cee8b
Dharun <50405830+Dharun-A@users.noreply.github.com>
2019-05-21 14:28:38 +0530

    Create Square_root_of_a_number.java

---
+++ Square root of a number/Square_root_of_a_number.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    double a = 3.0;
+	    double b = 4.0;
+	    double m = (a*a)+(b*b);
+	    double c = Math.sqrt(m);
+		System.out.println("The square root of "+m+" is "+c);
+	}
+}
